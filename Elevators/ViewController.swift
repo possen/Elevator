45659173b2c26ff348a87e01826cbcdde13c751f
Paul Ossenbruggen <possen@gmail.com>
2017-05-28 11:03:24 -0700

    Initial Commit

---
+++ Elevators/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Elevators
+//
+//  Created by Paul Ossenbruggen on 5/28/17.
+//  Copyright © 2017 Paul Ossenbruggen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
